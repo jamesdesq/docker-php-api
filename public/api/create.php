604e0904a617d7c7eeb00108009922ce5fd15127
James <james.dodd@airangel.com>
2023-04-26 19:49:36 +0100

    Add a function that will break the linter

--- public/api/create.php
+++ public/api/create.php
@@ -46,6 +46,10 @@ class PayPalPayments
         $this->_baseUrl = $_ENV['PAYPAL_CLIENT_ENDPOINT'];
     }
 
+    function justToTest() {
+        echo "just to test";
+    }
+
     /**
      * Creates the order
      *
